a668cf0ba1fc1453460be4e268e2c9f6ef3d51b2
Pete Rossman <pete.rossman@gmail.com>
2017-03-29 12:50:25 -0400

    First Commit

---
+++ GC_Lab3_SquareAndCubeCalculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC_Lab3_SquareAndCubeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt;
+            Console.WriteLine("Learn your squares and cubes!");
+            Console.WriteLine("");
+            Console.Write("Enter an integer: ");
+            Console.WriteLine("");
+            int.TryParse(Console.ReadLine(), out myInt);
+            Console.WriteLine("Number \tSquared  Cubed");
+            Console.WriteLine("====== \t=======  =====");
+            WriteOutput(myInt);
+            Console.ReadLine();
+        }
+
+        public static int Square(int num) => num * num;
+
+        public static int Cube(int num) => num * num * num;
+
+        public static void WriteOutput(int num)
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine(i + "\t" + Square(i) + "\t " + Cube(i));
+            }
+        }
+    }
+}
